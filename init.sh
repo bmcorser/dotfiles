95f5d474335b2a71a4341e0832a3736f28d0da43
ben <ben@monitor.(none)>
2012-07-07 03:08:05 +0100

    Context for init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-mv ~/.bashrc ~/.bashrc.old && ln -s dotfiles/bashrc ~/.bashrc
-mv ~/.fonts ~/.fonts.old && ln -s dotfiles/fonts ~/.fonts
-mv ~/.gitconfig ~/.gitconfig.old && ln -s dotfiles/gitconfig ~/.gitconfig
-mv ~/.vim ~/.vim.old && ln -s dotfiles/vim ~/.vim
-mv ~/.vimrc ~/.vimrc.old && ln -s dotfiles/vimrc ~/.vimrc
-mv ~/.Xmodmap ~/.Xmodmap.old && ln -s dotfiles/Xmodmap ~/.Xmodmap
+mv ~/.bashrc ~/.bashrc.old && ln -s bashrc ~/.bashrc
+mv ~/.fonts ~/.fonts.old && ln -s fonts ~/.fonts
+mv ~/.gitconfig ~/.gitconfig.old && ln -s gitconfig ~/.gitconfig
+mv ~/.vim ~/.vim.old && ln -s vim ~/.vim
+mv ~/.vimrc ~/.vimrc.old && ln -s vimrc ~/.vimrc
+mv ~/.Xmodmap ~/.Xmodmap.old && ln -s Xmodmap ~/.Xmodmap
 
 vim +BundleInstall +qa
