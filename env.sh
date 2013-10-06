90df6009a170ef4140b85f8e89661eda80a64e10
bmcorser <benmarshallcorser@gmail.com>
2013-10-06 10:29:44 +0100

    Rearrange

--- env.sh
+++ env.sh
@@ -1,3 +1,4 @@
+# aliases
 alias rmpyc='find . -name "*.pyc" -exec rm {} \;'
 alias cls='clear;echo "Currently logged in on $(tty), as ben in directory $(pwd)."'
 alias t='tmux -2 -u'
@@ -10,4 +11,6 @@ alias gcd='git checkout develop'
 alias ggpush!='git push origin $(current_branch) --force'
 alias grc='git rebase --continue'
 alias gmt='git mergetool'
-alias cnx='sshfs 192.168.0.9:/media/external ~/remote-external'
+
+# zsh env
+export DISABLE_AUTO_TITLE=true
